6a0c5966c1a5d4fe625311dcbb8641622ec159ef
Emiliano <e.munoz@alumno.um.edu.ar>
2024-03-12 19:01:47 -0300

    Modificación estructura

---
+++ boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/emiliano/enviroments/eden_env/bin/activate
+python3 app.py
---
+++ install.sh
@@ -0,0 +1,14 @@
+# Activar el entorno virtual
+source /home/emiliano/Escritorio/ing_software/ing_software/ing-soft-env/bin/activate
+
+# Verificar si el entorno virtual se activó correctamente
+if [ -n "$VIRTUAL_ENV" ]; then
+    echo "Entorno virtual activado con éxito."
+else
+    echo "No se pudo activar el entorno virtual."
+    exit 1
+fi
+
+# Instalar paquetes desde requirements.txt
+pip3 install -r requirements.txt
+
